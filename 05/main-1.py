bbedb94b17f6d47331d6278978fe50dfbceca2e0
Ruggero Tomaselli <ruggerotomaselli@gmail.com>
2022-12-05 09:24:16 +0100

    05

---
+++ 05/main-1.py
@@ -0,0 +1,30 @@
+import math
+
+data = open("input.txt").read().splitlines()
+
+nstacks = math.ceil(len(data[0]) / 4)
+
+stacks = [[] for i in range(nstacks)]
+
+while data[0] != "":
+  line = data.pop(0)
+  for i in range(nstacks):
+    crate = line[i*4+1:i*4+2]
+    if not crate.isupper():
+      continue
+    stacks[i].append(crate)
+
+data.pop(0)
+
+for line in data:
+  quantity = int(line[5:7])
+  start = int(line[12:14]) - 1
+  end = int(line[17:19]) - 1
+  for i in range(quantity):
+    stacks[end].insert(0, stacks[start].pop(0))
+
+final_str = ""
+for stack in stacks:
+  final_str += stack[0]
+
+print(final_str)
